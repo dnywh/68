5ccc3da9e18c357cf151ee90e45339011fb72de3
Danny White <danny@myfuturesuper.com.au>
2020-05-09 16:03:27 +1000

    basic style improvement

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
+// Bring in advice as array of strings
 import { adviceArray } from "/advice.js";
 
+// Target the element to house the chosen string
 const element = document.getElementById("advice");
 
+// Get a random item from advice array
 let randomItem = adviceArray[Math.floor(Math.random() * adviceArray.length)];
+// Add random item to element for page load
 element.textContent = randomItem;
-// console.log(randomItem);
 
-// 1. Create the button
-const button = document.createElement("button");
-button.innerHTML = "↑ Show me another";
-button.classList.add("button", "primary");
+// Prepare the secondary button for later
+const backUpButton = document.getElementById("back-up-button");
 
-// 2. Append somewhere
+// 1. Create the main button
+const showAnotherButton = document.createElement("button");
+showAnotherButton.innerHTML = "↑ Show me another";
+showAnotherButton.classList.add("button", "primary");
+
+// 2. Append main button to page
 const learnMoreButton = document.getElementById("learn-more-button");
 const buttonContainer = document.getElementById("button-container");
-buttonContainer.appendChild(button);
-button.after(learnMoreButton);
+buttonContainer.appendChild(showAnotherButton);
+showAnotherButton.after(learnMoreButton);
 
-// 3. Add event handler
-button.addEventListener("click", function () {
+// 3. Prepare function to handle getting a random string
+function showRandomAdvice() {
   randomItem = adviceArray[Math.floor(Math.random() * adviceArray.length)];
   element.textContent = randomItem;
-  button.innerHTML = "↑ And another";
-});
+  showAnotherButton.innerHTML = "↑ And another";
+}
+
+// 4. Add event handlers to relevant buttons
+showAnotherButton.addEventListener("click", showRandomAdvice);
+backUpButton.addEventListener("click", showRandomAdvice);
